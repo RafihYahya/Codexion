d52b098d4841a4e1b8d8f491c99f9d0f89c8a1da
RafihYahya <rafihyahya@gmail.com>
2026-02-20 22:58:54 +0000

    ..

--- coders/queue.c
+++ coders/queue.c
@@ -6,12 +6,14 @@
 /*   By: yrafih <yrafih@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 17:33:11 by yrafih            #+#    #+#             */
-/*   Updated: 2026/02/20 22:38:38 by yrafih           ###   ########.fr       */
+/*   Updated: 2026/02/20 22:58:26 by yrafih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "queue.h"
+#include "main.h"
+#include <stdlib.h>
 #include <unistd.h>
 
 int init_queue(struct s_Queue **q, void *value, struct s_Queue *next)
@@ -28,5 +30,19 @@ int init_queue(struct s_Queue **q, void *value, struct s_Queue *next)
 
 int append_to_queue(struct s_Queue **q, void *value)
 {
-	
+	struct s_Queue *tmp;
+
+	if (!q || !*q)
+		return (-1);
+	tmp = *q;
+	while (tmp->next) {
+		tmp = tmp->next;
+	}
+	tmp = malloc(sizeof(struct s_Queue));
+	if (!tmp) {
+		ERROR("Allocation Failed");
+		return (-1);
+	}
+	tmp->value = value;
+	tmp->next = NULL;
 }
